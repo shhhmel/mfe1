3da58d5d2df5583a067d79921a7553a95b22d81c
Ivan Myronov <gmironovivan1992@gmail.com>
2022-03-19 22:05:55 +0200

    Linking multiple apps together

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const mount = (el) => {
+  ReactDOM.render(<h1>Hi from Marketing</h1>, el);
+};
+
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector("#_marketing-dev-root");
+
+  if (el) {
+    mount(el);
+  }
+}
+
+export { mount };
