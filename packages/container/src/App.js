8e37fd14b0e25ed6f209534653a8770423dd1aba
Ivan Myronov <gmironovivan1992@gmail.com>
2022-03-20 10:30:56 +0200

    Generic Ties Between Projects

---
+++ packages/container/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import MarketingApp from "./components/MarketingApp";
+
+export default () => {
+  return (
+    <div>
+      <h1>Hello from container</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
+};
