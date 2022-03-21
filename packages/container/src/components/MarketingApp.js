ed6dbb68d8b2a5687b14081cb22594684e7ce86e
Ivan Myronov <gmironovivan1992@gmail.com>
2022-03-21 20:05:31 +0200

    Implementing Multi-Tier Navigation

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,11 +1,25 @@
 import React, { useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 import { mount } from "marketing/MarketingApp";
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
-  useEffect(() => mount(ref.current));
+  useEffect(() => {
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathName }) => {
+        const pathname = history.location;
+
+        if (pathname !== nextPathName) {
+          history.push(nextPathName);
+        }
+      },
+    });
+
+    history.listen(onParentNavigate);
+  });
 
   return <div ref={ref}></div>;
 };
