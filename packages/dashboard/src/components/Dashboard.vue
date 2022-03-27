9fa5db0e556040cd381ae20bf3a08c91cc07dfa9
Ivan Myronov <gmironovivan1992@gmail.com>
2022-03-27 12:14:25 +0300

    Update title in Dashboard App

--- packages/dashboard/src/components/Dashboard.vue
+++ packages/dashboard/src/components/Dashboard.vue
@@ -2,7 +2,7 @@
   <div class="p-grid p-fluid dashboard">
     <div class="p-col-12 p-lg-4">
       <div class="card summary">
-        <span class="title">Users</span>
+        <span class="title">Users !</span>
         <span class="detail">Number of visitors</span>
         <span class="count visitors">12</span>
       </div>
@@ -253,20 +253,20 @@
 </template>
 
 <script>
-import 'primevue/resources/themes/saga-blue/theme.css';
-import 'primevue/resources/primevue.css';
-import 'primeflex/primeflex.css';
-import 'primeicons/primeicons.css';
+import "primevue/resources/themes/saga-blue/theme.css";
+import "primevue/resources/primevue.css";
+import "primeflex/primeflex.css";
+import "primeicons/primeicons.css";
 
-import Checkbox from 'primevue/checkbox';
-import Panel from 'primevue/panel';
-import Dropdown from 'primevue/dropdown';
-import InputText from 'primevue/inputtext';
-import Button from 'primevue/button';
-import Column from 'primevue/column';
-import DataTable from 'primevue/datatable';
-import Chart from 'primevue/chart';
-import ProgressBar from 'primevue/progressbar';
+import Checkbox from "primevue/checkbox";
+import Panel from "primevue/panel";
+import Dropdown from "primevue/dropdown";
+import InputText from "primevue/inputtext";
+import Button from "primevue/button";
+import Column from "primevue/column";
+import DataTable from "primevue/datatable";
+import Chart from "primevue/chart";
+import ProgressBar from "primevue/progressbar";
 
 export default {
   components: {
@@ -284,19 +284,19 @@ export default {
     return {
       tasksCheckbox: [],
       dropdownCities: [
-        { name: 'New York', code: 'NY' },
-        { name: 'Rome', code: 'RM' },
-        { name: 'London', code: 'LDN' },
-        { name: 'Istanbul', code: 'IST' },
-        { name: 'Paris', code: 'PRS' },
+        { name: "New York", code: "NY" },
+        { name: "Rome", code: "RM" },
+        { name: "London", code: "LDN" },
+        { name: "Istanbul", code: "IST" },
+        { name: "Paris", code: "PRS" },
       ],
       dropdownCity: null,
       options: {
-        defaultDate: '2019-01-01',
+        defaultDate: "2019-01-01",
         header: {
-          left: 'prev,next',
-          center: 'title',
-          right: 'dayGridMonth,timeGridWeek,timeGridDay',
+          left: "prev,next",
+          center: "title",
+          right: "dayGridMonth,timeGridWeek,timeGridDay",
         },
         editable: true,
       },
@@ -305,28 +305,28 @@ export default {
       selectedProducts: null,
       lineData: {
         labels: [
-          'January',
-          'February',
-          'March',
-          'April',
-          'May',
-          'June',
-          'July',
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
         ],
         datasets: [
           {
-            label: 'First Dataset',
+            label: "First Dataset",
             data: [65, 59, 80, 81, 56, 55, 40],
             fill: false,
-            backgroundColor: '#2f4860',
-            borderColor: '#2f4860',
+            backgroundColor: "#2f4860",
+            borderColor: "#2f4860",
           },
           {
-            label: 'Second Dataset',
+            label: "Second Dataset",
             data: [28, 48, 40, 19, 86, 27, 90],
             fill: false,
-            backgroundColor: '#00bb7e',
-            borderColor: '#00bb7e',
+            backgroundColor: "#00bb7e",
+            borderColor: "#00bb7e",
           },
         ],
       },
@@ -334,9 +334,9 @@ export default {
   },
   methods: {
     formatCurrency(value) {
-      return value.toLocaleString('en-US', {
-        style: 'currency',
-        currency: 'USD',
+      return value.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
       });
     },
   },
@@ -514,7 +514,7 @@ h6 {
 
 @mixin clearfix {
   &:after {
-    content: '';
+    content: "";
     display: table;
     clear: both;
   }
@@ -549,7 +549,7 @@ p {
 
 /* Code Highlight */
 .docs {
-  pre[class*='language-'] {
+  pre[class*="language-"] {
     padding: 0 !important;
     background: transparent;
     overflow: visible;
